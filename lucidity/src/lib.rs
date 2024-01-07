9601e6482a2ed697a5b8140ac6cea50c217d5b84
Aaron Roney <twitchax@gmail.com>
2024-01-07 01:08:18 -0800

    Fix for docs.

--- lucidity/src/lib.rs
+++ lucidity/src/lib.rs
@@ -1,12 +1,11 @@
 //! The `lucidity` crate.  Re-exports the core components and provides a macro for creating jobs.
-#![doc = include_str!("../../README.md")]
+//#![doc = include_str!("../../README.md")]
 #![warn(rustdoc::broken_intra_doc_links, rust_2018_idioms, clippy::all, missing_docs)]
 
 pub use lucidity_core::Job;
 pub use lucidity_macros::job;
 
 pub use lucidity_core::lunatic;
-//pub use lucidity_core::lunatic_log;
 pub use lucidity_core::rand;
 pub use lucidity_core::serde;
 
